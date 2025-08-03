66f13db69bb79c844c095d4d36285ff2ca8d6cee
AlexKulam <alexkulam45@gmail.com>
2025-08-03 12:03:01 +0300

    добавлены схемы валидации данных

---
+++ backend/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class TaskCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    is_completed: bool
+
+    class Config:
+        orm_mode = True
