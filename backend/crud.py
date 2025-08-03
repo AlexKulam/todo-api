ba0e729956d05936331d09fbb4d1595f249a2ccc
AlexKulam <alexkulam45@gmail.com>
2025-08-03 12:01:55 +0300

    добавлены операции с тасками

---
+++ backend/crud.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from backend import models, schemas
+
+def create_task(db: Session, task: schemas.TaskCreate, user_id: int):
+    db_task = models.Task(**task.dict(), user_id=user_id)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def get_user_tasks(db: Session, user_id: int):
+    return db.query(models.Task).filter(models.Task.user_id == user_id).all()
+
+def update_task(db: Session, task_id: int, task_data: schemas.TaskUpdate):
+    task = db.query(models.Task).get(task_id)
+    if not task:
+        return None
+    for key, value in task_data.dict(exclude_unset=True).items():
+        setattr(task, key, value)
+    db.commit()
+    db.refresh(task)
+    return task
